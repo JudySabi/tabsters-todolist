2f672659977629650310fdd4689b78280ef5a7f1
Julie Mahe <j-mahe@hotmail.fr>
2021-02-15 21:12:10 +0100

    finishing sort

--- src/Components/ListTask.js
+++ src/Components/ListTask.js
@@ -3,25 +3,29 @@ import Task from "./Task";
 const ListTask = ({ listTask, filters, setListTask }) => {
   const { completed, inProgress, todo } = filters.status;
   const { alpha } = filters;
-  console.log(listTask);
+  {
+    // si le switch est true alors il trie les tasks selon le name
+
+    alpha
+      ? listTask.sort((a, b) => (a.name > b.name ? 1 : -1))
+      : listTask.sort((a, b) => (a.id > b.id ? 1 : -1));
+  }
   return (
     <div class="list-task row row-cols-1 row-cols-md-3 g-4">
-      {/* si le switch est true alors il trie le tableau */}
-      {alpha && listTask.sort((a, b) => (a.name > b.name ? 1 : -1))}
       {/* map les tasks et retourne status est true */}
       {listTask.map((elem) => {
         return (
-          ((completed === "Completed") & (elem.status === "Completed") && (
+          (completed === "Completed" && elem.status === "Completed" && (
             <div key={elem.id}>
               <Task data={elem} />
             </div>
           )) ||
-          ((inProgress === "In Progress") & (elem.status === "In Progress") && (
+          (inProgress === "In Progress" && elem.status === "In Progress" && (
             <div key={elem.id}>
               <Task data={elem} />
             </div>
           )) ||
-          ((todo === "To Do") & (elem.status === "To Do") && (
+          (todo === "To Do" && elem.status === "To Do" && (
             <div key={elem.id}>
               <Task data={elem} />
             </div>
